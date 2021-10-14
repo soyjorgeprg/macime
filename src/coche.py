860865b007d9eec734564706321f70f98b9b6e4b
Jorge Prieto <jorgeprg@correo.ugr.es>
2021-10-14 13:30:03 +0200

    Close #8, creación de las primeras clases

---
+++ src/coche.py
@@ -0,0 +1,10 @@
+class Coche:
+
+    def __init__(self, marca, modelo, consumo, deposito, actual)
+        pass
+
+    def modConsumo(self, consumo)
+        pass
+
+    def modActual(self, actual)
+        pass
---
+++ src/usuario.py
@@ -0,0 +1,12 @@
+import coche from Coche
+
+class Usuario:
+
+    def __init__(self, nombre, email)
+        pass
+
+    def anyadirCoche(self, coche)
+        pass
+
+    def modCoche(self, idCoche)
+        pass
