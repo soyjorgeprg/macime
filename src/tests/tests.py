c19938761080411a9264ce6754acf2290563ddab
Jorge Prieto <jorgeprg@correo.ugr.es>
2021-11-04 12:07:18 +0100

    Comprobaciones sobre la gestion de las gasolineras y refactorizacion de los viejos tests #16

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,46 +1,63 @@
 import sys
+import unittest
+import random
+import pathlib
+
 sys.path.insert(1, 'src')
 
 from gasolinera import Gasolinera
 from localizacion import Localizacion
-from principal import distanciaMinima 
 
-
-import unittest
-import random
 
 class Pruebas(unittest.TestCase):
 
     def setUp(self):
-        print("Preparando el contexto...")
-        self.l = Localizacion("Calle Atenas 13", "28850", "Torrejon de Ardoz", "Madrid", 40.452929045397944, -3.459447353367767)
-        self.L = Localizacion("Calle Espronceda 3", "46520", "Puerto de Sagunto", "Valencia", 39.65821250951125, -0.21494956814433297)
+        print("Montando entorno...")
+        self.casa = Localizacion("Calle Forja 22", "28850", "Torrejon de Ardoz", "Madrid", 40.4529061660252, -3.459435975099579)
+        self.tj = Localizacion("Calle Atenas 13", "28850", "Torrejon de Ardoz", "Madrid", 40.452929045397944, -3.459447353367767)
+        self.sg = Localizacion("Calle Espronceda 3", "46520", "Puerto de Sagunto", "Valencia", 39.65821250951125, -0.21494956814433297)
         self.gasolineras = []
-        
-        self.gasolineras.append(Gasolinera("CEPSA", round(random.uniform(0.65, 0.89), 2), self.l, ""))
-        self.gasolineras.append(Gasolinera("BP", round(random.uniform(0.65, 0.89), 2), self.L, ""))
 
+        self.gasolineras.append(Gasolinera("CEPSA", round(random.uniform(0.65, 0.89), 2), self.tj, ""))
+        self.gasolineras.append(Gasolinera("BP", round(random.uniform(0.65, 0.89), 2), self.sg, ""))
 
     def test_cargas(self):
-        # Testing if all the object Localizacion was correctly created
-        self.assertEqual(type(self.gasolineras[0].getLocalizacion()), type(self.l))
-        self.assertEqual(self.gasolineras[1].getLocalizacion(), self.L)
+        '''
+            Testing if all the object Localizacion was correctly created
+        '''
+        self.assertIsInstance(self.gasolineras[0].getLocalizacion(), type(self.tj))
+        self.assertEqual(self.gasolineras[1].getLocalizacion(), self.sg)
 
-    
     def test_localizacion(self):
-        # Testing it returns a tuple of floats
-        self.assertEqual(type(self.l.getLocalizacionPrecisa()), type((float, float)))
-        
+        '''
+            Testing if it returns a tuple
+        '''
+        self.assertIsInstance(self.tj.getLocalizacionPrecisa(), tuple)
 
     def test_distancia(self):
-        x = distanciaMinima("Calle Forja 22 28850 Torrejon de Ardoz", self.gasolineras)
-
+        '''
+            Checking if it calculates distance correctly
+        '''
+        x = self.casa.distanciaMinima(self.gasolineras)
         self.assertEqual(x, 0)
 
-    
+    def test_gestiongasolineras(self):
+        '''
+            Checking if it get the json
+        '''
+        path = "src/data/glp.json"
+        if not pathlib.Path(path).resolve().is_file():
+            raise AssertionError("No existe el fichero: %s" % path)
+        else:
+            g = Gasolinera()
+            eess = g.leerGasolineras("GLP")
+            self.assertIsInstance(eess, list)
+            self.assertIsInstance(eess[0], type(g))
+            self.assertIsInstance(eess[0].getLocalizacion(), type(self.tj))
+
     def tearDown(self):
-        print("Limpiando contexto")
-        del(self.gasolineras)
+        print("Destruyendo entorno...")
+        del self.gasolineras
 
     if __name__ == "__main__":
         unittest.main()
