046a3f165b3442300188cf5b52b817073cd2ad5e
Jorge Prieto <jorgeprg@correo.ugr.es>
2021-11-16 19:26:14 +0100

    #24 Ruta del fichero de resultado
    Escribo en /tmp porque en todos los sistema _deberia_ tener permisos para escribir

--- dodo.py
+++ dodo.py
@@ -2,7 +2,7 @@ from doit.action import CmdAction
 
 DOIT_CONFIG = {
         'backend': 'json',
-        'dep_file': '/app/test/doit-db.json',
+        'dep_file': '/tmp/doit-db.json',
     }
 
 def task_pruebas():
